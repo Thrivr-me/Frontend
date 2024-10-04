75d05f0f094df7e5a31fd22e8523de414a2b9a0c
Ealfred1 <alfrederic371@gmail.com>
2024-10-04 20:46:57 +0100

    constants and navigation section preparations

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import Layout from './Layout'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Button from './Button'
+import Hero from './LandingPage/Hero'
 import NotFound from './NotFound'
 
 export {
 	Layout,
 	Navbar,
+	Hero,
 	Footer,
 	NotFound,
 	Button
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Hero } from '../components/'
 
 const LandingPage = () => {
 	return (
-		<div>
-			j
+		<div className="overflow-hidden">
+			<Hero />
 		</div>
 	)
 }
