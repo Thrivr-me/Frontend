31240a42a2893d6c4097f87f9034cb987b9e4b9d
Ealfred1 <alfrederic371@gmail.com>
2024-10-04 20:42:17 +0100

    constants and navigation section preparations

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Layout from './Layout'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Button from './Button'
 import NotFound from './NotFound'
 
 export {
 	Layout,
 	Navbar,
 	Footer,
-	NotFound
+	NotFound,
+	Button
 }
